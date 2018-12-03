92d16b054cf2b5ef6437dbaed161af1ed27a830d
whaT <golovan.k@yandex.ru>
2018-12-03 11:12:57 +0300

    create basics tables model

---
+++ server/CasualServer/CasualServer/Models/UserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasualServer.Models
+{
+    public class UserService
+    {
+        public int UserServiceId { get; set; }
+        public int User { get; set; }
+        public int[] Services { get; set; }
+    }
+}

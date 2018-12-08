748e4f7ada35eba2109e70d426e3b3a8e5777e9e
whaT <golovan.k@yandex.ru>
2018-12-08 18:12:28 +0300

    create a basic structure of project and add auth

---
+++ client/CasualClient/CasualClient/ViewModels/AuthVM.cs
@@ -0,0 +1,98 @@
+﻿using CasualClient.Views;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CasualClient.ViewModels
+{
+    class AuthVM : BaseVM
+    {
+        private string _login;
+
+        public string Login
+        {
+            get { return _login; }
+            set { _login = value; RaisePropertyChanged("Login"); }
+        }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; RaisePropertyChanged("Password"); }
+        }
+        public DelegateCommand<Window> OpenMainWindow
+        {
+            get
+            {
+                return new DelegateCommand<Window>((w) =>
+                {
+                    bool logPassIsValid = CheckLogPass();
+                    if(logPassIsValid)
+                    {
+                        MainWindow mw = new MainWindow();
+                        MainWindowVM mwVm = new MainWindowVM();
+                        mw.DataContext = mwVm;
+                        w.Close();
+                        mw.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Логин или пароль введены неверно");
+                    }
+                });
+            }
+        }
+        private bool CheckLogPass()
+        {
+            
+            //////////////////////////////////////////////////////
+            WebRequest request = WebRequest.Create("http://localhost:5000/users/check");
+            request.Method = "GET";
+            request.Headers.Add("login", Login);
+            request.Headers.Add("password", Password);
+            // устанавливаем тип содержимого - параметр ContentType
+            request.ContentType = "application/json";
+
+            string result;
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            if (result == "true")
+                return true;
+            else
+                return false;
+        }
+
+        public DelegateCommand<Window> OpenRegisterWindow
+        {
+            get
+            {
+                return new DelegateCommand<Window>((w) =>
+                {
+
+                    RegisterWindow rw = new RegisterWindow();
+                    RegisterWindowVM rwVm = new RegisterWindowVM();
+                    rw.DataContext = rwVm;
+                    w.Close();
+                    rw.Show();
+
+                });
+            }
+        }
+
+
+    }
+}
---
+++ client/CasualClient/CasualClient/ViewModels/BaseVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace CasualClient.ViewModels
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
